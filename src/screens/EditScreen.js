8b776b2b62425a68f2966e97d60f326db56736ed
Clémence LACAILLE <57369417+Clemence-Lacaille@users.noreply.github.com>
2021-12-08 10:40:54 +0000

    Edit and add

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const EditScreen = () => {
+    return (
+        <View>
+            <Text>Edit</Text>
+        </View>
+    )
+}
+
+export default EditScreen;
+
